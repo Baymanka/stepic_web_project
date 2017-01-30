2a029a08b38b7d76138af639bf6f8ea892383b04
Baymanka <alexeylisitsyn31@gmail.com>
2017-01-30 11:23:15 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,7 +1,2 @@
-def app (environ, start_response):
-  status = '200 OK'
-  response_headers = [('Content-Type','text/plain')]
-  start_response(status, response_headers)
-  resp = environ['QUERY_STRING'].split("&")
-  resp = [item+"\r\n" for item in resp]
-  return resp
+pythonpath = '/home/box/web'
+bind = "0.0.0.0:8080"
